64726c04d1214e0a77d924b04b210df15d589456
Kevin Simpson <ktsimpso@gmail.com>
2022-12-01 23:31:58 -0800

    implemented day2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,12 +1,7 @@
-use adventofcode2022::{
-    parse_lines, parse_usize, Problem, ProblemWithOnePart, ProblemWithTwoParts,
-};
+use adventofcode2022::{parse_lines, Problem, ProblemWithTwoParts};
 use bpaf::{construct, short, Parser as ArgParser};
-use chumsky::{
-    prelude::Simple,
-    primitive::{filter, just},
-    Parser,
-};
+use chumsky::{prelude::Simple, primitive::just, Parser};
+use strum_macros::EnumString;
 
 #[derive(Debug, Clone)]
 pub enum Roshambo {
@@ -15,9 +10,107 @@ pub enum Roshambo {
     Scissors,
 }
 
-type ParseOutput = Vec<(Roshambo, Roshambo)>;
+impl Roshambo {
+    fn get_score(&self) -> usize {
+        match self {
+            Roshambo::Rock => 1,
+            Roshambo::Paper => 2,
+            Roshambo::Scissors => 3,
+        }
+    }
 
-pub const DAY_02: ProblemWithOnePart<Arguments, ParseOutput, usize> = Problem::new(
+    fn get_outcome(&self, opponents_move: &Roshambo) -> Outcome {
+        match (opponents_move, self) {
+            (Roshambo::Rock, Roshambo::Paper) => Outcome::Win,
+            (Roshambo::Rock, Roshambo::Scissors) => Outcome::Lose,
+            (Roshambo::Paper, Roshambo::Rock) => Outcome::Lose,
+            (Roshambo::Paper, Roshambo::Scissors) => Outcome::Win,
+            (Roshambo::Scissors, Roshambo::Rock) => Outcome::Win,
+            (Roshambo::Scissors, Roshambo::Paper) => Outcome::Lose,
+            _ => Outcome::Draw,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl Outcome {
+    fn get_score(&self) -> usize {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Lose => 0,
+            Outcome::Draw => 3,
+        }
+    }
+
+    fn get_roshambo(&self, opponents_move: &Roshambo) -> Roshambo {
+        match (opponents_move, self) {
+            (Roshambo::Rock, Outcome::Win) => Roshambo::Paper,
+            (Roshambo::Rock, Outcome::Lose) => Roshambo::Scissors,
+            (Roshambo::Paper, Outcome::Win) => Roshambo::Scissors,
+            (Roshambo::Paper, Outcome::Lose) => Roshambo::Rock,
+            (Roshambo::Scissors, Outcome::Win) => Roshambo::Rock,
+            (Roshambo::Scissors, Outcome::Lose) => Roshambo::Paper,
+            (a, Outcome::Draw) => a.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum StrategyKey {
+    X,
+    Y,
+    Z,
+}
+
+impl StrategyKey {
+    fn to_roshambo(&self) -> Roshambo {
+        match self {
+            StrategyKey::X => Roshambo::Rock,
+            StrategyKey::Y => Roshambo::Paper,
+            StrategyKey::Z => Roshambo::Scissors,
+        }
+    }
+
+    fn to_outcome(&self) -> Outcome {
+        match self {
+            StrategyKey::X => Outcome::Lose,
+            StrategyKey::Y => Outcome::Draw,
+            StrategyKey::Z => Outcome::Win,
+        }
+    }
+}
+
+#[derive(Debug, Clone, EnumString)]
+pub enum Strategy {
+    Roshambo,
+    Outcome,
+}
+
+trait Round {
+    fn get_round_result(&self) -> usize;
+}
+
+impl Round for (Roshambo, Roshambo) {
+    fn get_round_result(&self) -> usize {
+        self.1.get_outcome(&self.0).get_score() + self.1.get_score()
+    }
+}
+
+impl Round for (Roshambo, Outcome) {
+    fn get_round_result(&self) -> usize {
+        self.1.get_roshambo(&self.0).get_score() + self.1.get_score()
+    }
+}
+
+type ParseOutput = Vec<(Roshambo, StrategyKey)>;
+
+pub const DAY_02: ProblemWithTwoParts<Arguments, ParseOutput, usize> = Problem::new(
     "day02",
     "Parses and scores a secret strategy for a rock paper scissors tournament",
     "Path to the input file. Input should be lines with value A, B, or C followed by X, Y, or Z separated by a space. The first character represents the opponents move and the second is our strategy",
@@ -25,13 +118,20 @@ pub const DAY_02: ProblemWithOnePart<Arguments, ParseOutput, usize> = Problem::n
     parse_file,
     run,
 )
-.with_part1(Arguments {}, "Our strategy is a certain value of Rock, Paper, or Scissors to use");
+.with_part1(Arguments { strategy: Strategy::Roshambo }, "Our strategy is a certain value of Rock, Paper, or Scissors to use")
+.with_part2(Arguments { strategy: Strategy::Outcome }, "Our strategy is a target outcome to have");
 
 #[derive(Debug, Clone)]
-pub struct Arguments {}
+pub struct Arguments {
+    strategy: Strategy,
+}
 
 fn parse_arguments() -> Box<dyn ArgParser<Arguments>> {
-    Box::new(construct!(Arguments {}))
+    let strategy = short('s')
+        .long("strategy")
+        .help("The strategy to use in the game")
+        .argument::<Strategy>("STRATEGY");
+    Box::new(construct!(Arguments { strategy }))
 }
 
 fn parse_file(file: String) -> ParseOutput {
@@ -42,33 +142,34 @@ fn parser() -> impl Parser<char, ParseOutput, Error = Simple<char>> {
     parse_lines(parse_game())
 }
 
-fn parse_game() -> impl Parser<char, (Roshambo, Roshambo), Error = Simple<char>> {
+fn parse_game() -> impl Parser<char, (Roshambo, StrategyKey), Error = Simple<char>> {
     parse_roshambo()
         .then_ignore(just(' '))
-        .then(parse_roshambo())
+        .then(parse_strategy())
 }
 
 fn parse_roshambo() -> impl Parser<char, Roshambo, Error = Simple<char>> {
-    let rock = just('A').or(just('X')).to(Roshambo::Rock);
-    let paper = just('B').or(just('Y')).to(Roshambo::Paper);
-    let scissors = just('C').or(just('Z')).to(Roshambo::Scissors);
+    let rock = just('A').to(Roshambo::Rock);
+    let paper = just('B').to(Roshambo::Paper);
+    let scissors = just('C').to(Roshambo::Scissors);
 
     rock.or(paper).or(scissors)
+}
+
+fn parse_strategy() -> impl Parser<char, StrategyKey, Error = Simple<char>> {
+    let lose = just('X').to(StrategyKey::X);
+    let draw = just('Y').to(StrategyKey::Y);
+    let win = just('Z').to(StrategyKey::Z);
+
+    lose.or(draw).or(win)
 }
 
 fn run(input: ParseOutput, arguments: Arguments) -> usize {
     let score = input
         .into_iter()
-        .map(|(p1, p2)| match (p1, p2) {
-            (Roshambo::Rock, Roshambo::Rock) => 3 + 1,
-            (Roshambo::Rock, Roshambo::Paper) => 6 + 2,
-            (Roshambo::Rock, Roshambo::Scissors) => 0 + 3,
-            (Roshambo::Paper, Roshambo::Rock) => 0 + 1,
-            (Roshambo::Paper, Roshambo::Paper) => 3 + 2,
-            (Roshambo::Paper, Roshambo::Scissors) => 6 + 3,
-            (Roshambo::Scissors, Roshambo::Rock) => 6 + 1,
-            (Roshambo::Scissors, Roshambo::Paper) => 0 + 2,
-            (Roshambo::Scissors, Roshambo::Scissors) => 3 + 3,
+        .map(|(opponents_move, strategy_key)| match arguments.strategy {
+            Strategy::Roshambo => (opponents_move, strategy_key.to_roshambo()).get_round_result(),
+            Strategy::Outcome => (opponents_move, strategy_key.to_outcome()).get_round_result(),
         })
         .sum();
 
