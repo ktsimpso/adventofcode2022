0cb8fec17f5e7bd3b6567e8bd84a98dadfb989ba
Kevin Simpson <ktsimpso@gmail.com>
2022-12-04 22:18:06 -0800

    Completed Day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,11 +1,9 @@
-use adventofcode2022::{
-    parse_between_blank_lines, parse_lines, parse_usize, single_arg, Command, ParseError, Problem,
-};
+use adventofcode2022::{flag_arg, parse_lines, parse_usize, Command, ParseError, Problem};
 use anyhow::Result;
 use chumsky::{
     prelude::Simple,
-    primitive::{end, just, one_of, todo},
-    text, Parser,
+    primitive::{end, just, one_of},
+    Parser,
 };
 use clap::ArgMatches;
 use std::cell::LazyCell;
@@ -13,28 +11,36 @@ use std::cell::LazyCell;
 type ParseOutput = (Vec<Vec<Option<char>>>, Vec<(usize, usize, usize)>);
 
 pub const DAY_05: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
-    //let number = single_arg("number", 'n', "The number of elves to sum")
-    //    .value_parser(clap::value_parser!(usize));
+    let upgraded_crane = flag_arg(
+        "upgraded_crane",
+        'u',
+        "If the crane can move more than 1 box at a time",
+    );
+
     let problem = Problem::new(
         "day05",
         "Takes the current stacks as well as crane instructions for how to move boxes between stacks. Then returns the top of each stack after all moves",
         "Path to the input file. File should consist of the stacks, followed by a blank line, then newline delimited move instructions.",
-        vec![],
+        vec![upgraded_crane],
         parse_arguments,
         parse_file,
         run,
     )
-    .with_part1(CommandLineArguments {}, "The crane moves each box one at a time.");
-    //.with_part2(CommandLineArguments {}, "part 2 help");
+    .with_part1(CommandLineArguments { upgraded_crane: false }, "The crane moves each box one at a time.")
+    .with_part2(CommandLineArguments { upgraded_crane: true }, "part 2 help");
     Box::new(problem)
 });
 
 #[derive(Debug, Clone)]
-pub struct CommandLineArguments {}
+pub struct CommandLineArguments {
+    upgraded_crane: bool,
+}
 
 fn parse_arguments(args: &ArgMatches) -> CommandLineArguments {
     CommandLineArguments {
-        //n: *args.get_one::<usize>("number").expect("Valid arguments"),
+        upgraded_crane: *args
+            .get_one::<bool>("upgraded_crane")
+            .expect("Valid arguments"),
     }
 }
 
@@ -95,6 +101,11 @@ fn run(input: ParseOutput, arguments: CommandLineArguments) -> String {
             let mut popped = (0..m)
                 .map(|_| from.pop().expect("Non empty stack"))
                 .collect::<Vec<char>>();
+
+            if arguments.upgraded_crane {
+                popped.reverse();
+            }
+
             let to = stacks.get_mut(t).expect("stack exists");
             to.append(&mut popped);
         });
