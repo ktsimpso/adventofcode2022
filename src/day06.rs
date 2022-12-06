9fe6c762ac6cf834d9ec9ea77b7d909c03f4aa11
Kevin Simpson <ktsimpso@gmail.com>
2022-12-05 21:39:10 -0800

    Completed day 6 part 2.

--- src/day06.rs
+++ src/day06.rs
@@ -1,11 +1,7 @@
-use adventofcode2022::{
-    parse_between_blank_lines, parse_lines, parse_usize, single_arg, Command, ParseError, Problem,
-};
+use adventofcode2022::{single_arg, Command, ParseError, Problem};
 use anyhow::Result;
 use chumsky::{
-    chain::Chain,
     prelude::Simple,
-    primitive::todo,
     primitive::{end, take_until},
     text, Parser,
 };
@@ -15,31 +11,36 @@ use std::{cell::LazyCell, collections::BTreeSet};
 type ParseOutput = Vec<char>;
 
 pub const DAY_06: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
-    //let number = single_arg("number", 'n', "The number of elves to sum")
-    //    .value_parser(clap::value_parser!(usize));
+    let number = single_arg("number", 'n', "The number of unique characters to find")
+        .value_parser(clap::value_parser!(usize));
     let problem = Problem::new(
         "day06",
         "day6 finds unique sets of strings in a message packet.",
         "Path to the input file. Input should be one line for the message",
-        vec![],
+        vec![number],
         parse_arguments,
         parse_file,
         run,
     )
     .with_part1(
-        CommandLineArguments {},
-        "finds the first set of 4 unique characters in the input string.",
+        CommandLineArguments { n: 4 },
+        "Finds the first set of 4 unique characters in the input string.",
+    )
+    .with_part2(
+        CommandLineArguments { n: 14 },
+        "Finds the first set of 14 unique characters in the input string.",
     );
-    //.with_part2(CommandLineArguments {}, "part 2 help");
     Box::new(problem)
 });
 
 #[derive(Debug, Clone)]
-pub struct CommandLineArguments {}
+pub struct CommandLineArguments {
+    n: usize,
+}
 
 fn parse_arguments(args: &ArgMatches) -> CommandLineArguments {
     CommandLineArguments {
-        //n: *args.get_one::<usize>("number").expect("Valid arguments"),
+        n: *args.get_one::<usize>("number").expect("Valid arguments"),
     }
 }
 
@@ -57,11 +58,13 @@ fn parser() -> impl Parser<char, ParseOutput, Error = Simple<char>> {
 
 fn run(input: ParseOutput, arguments: CommandLineArguments) -> usize {
     input
-        .windows(4)
+        .windows(arguments.n)
         .into_iter()
         .enumerate()
-        .find(|(_, chars)| chars.into_iter().cloned().collect::<BTreeSet<char>>().len() == 4)
+        .find(|(_, chars)| {
+            chars.into_iter().cloned().collect::<BTreeSet<char>>().len() == arguments.n
+        })
         .map(|(position, _)| position)
         .expect("valid message")
-        + 4
+        + arguments.n
 }
