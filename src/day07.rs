b3e112c3da9c7d0746f840990da062bdfd145b28
Kevin Simpson <ktsimpso@gmail.com>
2022-12-07 10:10:50 -0800

    Completed day 1 part 1

--- src/day07.rs
+++ src/day07.rs
@@ -2,25 +2,31 @@ use adventofcode2022::{
     parse_between_blank_lines, parse_lines, parse_usize, single_arg, Command, ParseError, Problem,
 };
 use anyhow::Result;
-use chumsky::{prelude::Simple, primitive::end, primitive::todo, Parser};
+use chumsky::{
+    prelude::Simple,
+    primitive::end,
+    primitive::{just, take_until, todo},
+    text, Error, Parser,
+};
 use clap::ArgMatches;
+use itertools::Itertools;
 use std::cell::LazyCell;
 
-type ParseOutput = Vec<Vec<usize>>;
+type ParseOutput = Vec<TerminalOutput>;
 
 pub const DAY_07: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
     //let number = single_arg("number", 'n', "The number of elves to sum")
     //    .value_parser(clap::value_parser!(usize));
     let problem = Problem::new(
         "day07",
-        "day 7 help",
-        "Path to the input file. file heko",
+        "Reads terminal output then gives stats on the file size for folders found in the terminal output.",
+        "Path to the input file. The output of one terminal session of the elf computer.",
         vec![],
         parse_arguments,
         parse_file,
         run,
     )
-    .with_part1(CommandLineArguments {}, "part 1 help");
+    .with_part1(CommandLineArguments {}, "Finds all the folder with size less than 100_000 and sums their total.");
     //.with_part2(CommandLineArguments { }, "Part 2 help");
     Box::new(problem)
 });
@@ -28,6 +34,31 @@ pub const DAY_07: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
 #[derive(Debug, Clone)]
 pub struct CommandLineArguments {
     //n: usize,
+}
+
+#[derive(Debug, Clone)]
+pub enum FileCommand {
+    ChangeDirectory(DirectoryDirection),
+    List,
+}
+
+#[derive(Debug, Clone)]
+pub enum DirectoryDirection {
+    Root,
+    Down(String),
+    Up,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ElfFile {
+    Directory(String),
+    File(String, usize),
+}
+
+#[derive(Debug, Clone)]
+pub enum TerminalOutput {
+    FileCommand(FileCommand),
+    ElfFile(ElfFile),
 }
 
 fn parse_arguments(args: &ArgMatches) -> CommandLineArguments {
@@ -43,10 +74,152 @@ fn parse_file(file: String) -> Result<ParseOutput> {
 }
 
 fn parser() -> impl Parser<char, ParseOutput, Error = Simple<char>> {
-    parse_between_blank_lines(parse_lines(parse_usize())).then_ignore(end());
-    todo()
+    parse_lines(parse_terminal_output()).then_ignore(end())
+}
+
+fn parse_terminal_output() -> impl Parser<char, TerminalOutput, Error = Simple<char>> {
+    parse_file_command()
+        .map(|command| TerminalOutput::FileCommand(command))
+        .or(parse_elf_file().map(|file| TerminalOutput::ElfFile(file)))
+}
+
+fn parse_file_command() -> impl Parser<char, FileCommand, Error = Simple<char>> {
+    just("$ ").ignore_then(
+        just("ls")
+            .to(FileCommand::List)
+            .or(parse_directory_direction()
+                .map(|direction| FileCommand::ChangeDirectory(direction))),
+    )
+}
+
+fn parse_directory_direction() -> impl Parser<char, DirectoryDirection, Error = Simple<char>> {
+    let root = just("/").to(DirectoryDirection::Root);
+    let up = just("..").to(DirectoryDirection::Up);
+    let down = take_until(text::newline().rewind())
+        .map(|(name, _)| DirectoryDirection::Down(name.into_iter().collect()));
+    just("cd ").ignore_then(root.or(up).or(down))
+}
+
+fn parse_elf_file() -> impl Parser<char, ElfFile, Error = Simple<char>> {
+    parse_efile().or(parse_directory())
+}
+
+fn parse_efile() -> impl Parser<char, ElfFile, Error = Simple<char>> {
+    parse_usize()
+        .then_ignore(just(" "))
+        .then(take_until(text::newline().rewind()))
+        .map(|(size, (name, _))| ElfFile::File(name.into_iter().collect(), size))
+}
+
+fn parse_directory() -> impl Parser<char, ElfFile, Error = Simple<char>> {
+    just("dir")
+        .ignore_then(just(" "))
+        .ignore_then(take_until(text::newline().rewind()))
+        .map(|(name, _)| ElfFile::Directory(name.into_iter().collect()))
+}
+
+#[derive(Debug)]
+struct Arena {
+    files: Vec<FileSystem>,
+}
+
+#[derive(Debug, Clone)]
+struct FileSystem {
+    file: ElfFile,
+    parent: Option<usize>,
+    children: Vec<usize>,
 }
 
 fn run(input: ParseOutput, arguments: CommandLineArguments) -> usize {
-    0
+    let root = FileSystem {
+        file: ElfFile::Directory("/".to_string()),
+        parent: None,
+        children: Vec::new(),
+    };
+    let mut arena = Arena { files: vec![root] };
+    let mut current = 0usize;
+
+    input.into_iter().for_each(|output| match output {
+        TerminalOutput::ElfFile(file) => {
+            if arena
+                .files
+                .get(current)
+                .expect("valid index")
+                .children
+                .iter()
+                .find(|child| arena.files.get(**child).expect("valid index").file == file)
+                .is_none()
+            {
+                let new_file = FileSystem {
+                    file: file,
+                    parent: Some(current),
+                    children: Vec::new(),
+                };
+                arena.files.push(new_file);
+                let new_index = arena.files.len() - 1;
+                arena
+                    .files
+                    .get_mut(current)
+                    .expect("valid index")
+                    .children
+                    .push(new_index);
+            }
+        }
+        TerminalOutput::FileCommand(command) => match command {
+            FileCommand::List => (),
+            FileCommand::ChangeDirectory(direction) => match direction {
+                DirectoryDirection::Root => current = 0usize,
+                DirectoryDirection::Down(file_name) => {
+                    let child = arena
+                        .files
+                        .get(current)
+                        .expect("valid index")
+                        .children
+                        .iter()
+                        .find(
+                            |child| match &arena.files.get(**child).expect("valid index").file {
+                                ElfFile::Directory(name) => name == &file_name,
+                                _ => false,
+                            },
+                        )
+                        .expect("valid cd");
+                    current = *child
+                }
+                DirectoryDirection::Up => {
+                    current = arena
+                        .files
+                        .get(current)
+                        .expect("valid index")
+                        .parent
+                        .expect("valid parent")
+                }
+            },
+        },
+    });
+
+    arena
+        .files
+        .iter()
+        .filter(|file| match file.file {
+            ElfFile::Directory(_) => true,
+            _ => false,
+        })
+        .map(|dir| du(&arena, dir))
+        .filter(|value| *value <= 100_000)
+        .sum()
+}
+
+fn du(arena: &Arena, directory: &FileSystem) -> usize {
+    directory
+        .children
+        .iter()
+        .map(|file_index| {
+            let child = arena.files.get(*file_index).expect("Valid index");
+
+            match child.file {
+                ElfFile::Directory(_) => du(arena, child),
+                ElfFile::File(_, len) => len,
+            }
+        })
+        .sum()
 }
