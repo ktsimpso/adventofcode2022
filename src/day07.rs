f43f07bab89394c395fcbd336cab69dba7c4f12d
Kevin Simpson <ktsimpso@gmail.com>
2022-12-06 20:57:17 -0800

    Day 7 Skeleton

---
+++ src/day07.rs
@@ -0,0 +1,52 @@
+use adventofcode2022::{
+    parse_between_blank_lines, parse_lines, parse_usize, single_arg, Command, ParseError, Problem,
+};
+use anyhow::Result;
+use chumsky::{prelude::Simple, primitive::end, primitive::todo, Parser};
+use clap::ArgMatches;
+use std::cell::LazyCell;
+
+type ParseOutput = Vec<Vec<usize>>;
+
+pub const DAY_07: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
+    //let number = single_arg("number", 'n', "The number of elves to sum")
+    //    .value_parser(clap::value_parser!(usize));
+    let problem = Problem::new(
+        "day07",
+        "day 7 help",
+        "Path to the input file. file heko",
+        vec![],
+        parse_arguments,
+        parse_file,
+        run,
+    )
+    .with_part1(CommandLineArguments {}, "part 1 help");
+    //.with_part2(CommandLineArguments { }, "Part 2 help");
+    Box::new(problem)
+});
+
+#[derive(Debug, Clone)]
+pub struct CommandLineArguments {
+    //n: usize,
+}
+
+fn parse_arguments(args: &ArgMatches) -> CommandLineArguments {
+    CommandLineArguments {
+        //n: *args.get_one::<usize>("number").expect("Valid arguments"),
+    }
+}
+
+fn parse_file(file: String) -> Result<ParseOutput> {
+    parser()
+        .parse(file.clone())
+        .map_err(|e| ParseError(file, e).into())
+}
+
+fn parser() -> impl Parser<char, ParseOutput, Error = Simple<char>> {
+    parse_between_blank_lines(parse_lines(parse_usize())).then_ignore(end());
+    todo()
+}
+
+fn run(input: ParseOutput, arguments: CommandLineArguments) -> usize {
+    0
+}
