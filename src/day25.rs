8d8cdb5109f0e2477fc1739a3a11e6a20a628d4c
Kevin Simpson <kesimpso@linkedin.com>
2022-12-25 08:12:06 -0800

    Completed day 25.

--- src/day25.rs
+++ src/day25.rs
@@ -1,39 +1,47 @@
-use adventofcode2022::{
-    parse_between_blank_lines, parse_lines, parse_usize, single_arg, Command, ParseError, Problem,
-};
+use adventofcode2022::{parse_lines, Command, ParseError, Problem};
 use anyhow::Result;
-use chumsky::{prelude::Simple, primitive::end, Parser};
+use chumsky::{
+    prelude::Simple,
+    primitive::{end, just},
+    Parser,
+};
 use clap::ArgMatches;
-use std::cell::LazyCell;
+use itertools::Itertools;
+use std::{cell::LazyCell, fmt::Display};
 
-type ParseOutput = Vec<Vec<usize>>;
+type ParseOutput = Vec<SnafuNumber>;
 
 pub const DAY_25: LazyCell<Box<dyn Command>> = LazyCell::new(|| {
-    //let number = single_arg("number", 'n', "The number of elves to sum")
-    //    .value_parser(clap::value_parser!(usize));
     let problem = Problem::new(
         "day25",
-        "Day 25 help",
-        "Path to the input file. file help",
+        "Sums snafu numbers",
+        "Path to the input file. The snafu numbers to sum, one snafu number on each line.",
         vec![],
         parse_arguments,
         parse_file,
         run,
     )
-    .with_part1(CommandLineArguments {}, "part 1 help");
-    //.with_part2(CommandLineArguments { }, "part 2 help");
+    .with_part1(
+        CommandLineArguments {},
+        "Sums the snafu numbers for the default input.",
+    );
     Box::new(problem)
 });
 
 #[derive(Debug, Clone)]
-pub struct CommandLineArguments {
-    //n: usize,
+pub struct CommandLineArguments {}
+
+fn parse_arguments(_args: &ArgMatches) -> CommandLineArguments {
+    CommandLineArguments {}
 }
 
-fn parse_arguments(args: &ArgMatches) -> CommandLineArguments {
-    CommandLineArguments {
-        //n: *args.get_one::<usize>("number").expect("Valid arguments"),
-    }
+#[derive(Debug, Clone)]
+pub enum Snafu {
+    Two,
+    One,
+    Zero,
+    Minus,
+    DoubleMinus,
 }
 
 fn parse_file(file: String) -> Result<ParseOutput> {
@@ -43,10 +51,121 @@ fn parse_file(file: String) -> Result<ParseOutput> {
 }
 
 fn parser() -> impl Parser<char, ParseOutput, Error = Simple<char>> {
-    parse_between_blank_lines(parse_lines(parse_usize())).then_ignore(end())
+    parse_lines(
+        parse_snafu()
+            .repeated()
+            .at_least(1)
+            .map(|number| SnafuNumber(number)),
+    )
+    .then_ignore(end())
 }
 
-fn run(input: ParseOutput, arguments: CommandLineArguments) -> usize {
-    dbg!(input);
-    0
+fn parse_snafu() -> impl Parser<char, Snafu, Error = Simple<char>> {
+    let two = just('2').to(Snafu::Two);
+    let one = just('1').to(Snafu::One);
+    let zero = just('0').to(Snafu::Zero);
+    let minus = just('-').to(Snafu::Minus);
+    let double_minus = just('=').to(Snafu::DoubleMinus);
+
+    two.or(one).or(zero).or(minus).or(double_minus)
+}
+
+fn run(input: ParseOutput, _arguments: CommandLineArguments) -> String {
+    let result: SnafuNumber = input
+        .into_iter()
+        .map(|snafu| {
+            let integer: isize = snafu.into();
+            integer
+        })
+        .sum::<isize>()
+        .into();
+
+    result.to_string()
+}
+
+struct SnafuNumber(Vec<Snafu>);
+
+impl Display for SnafuNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result = self
+            .0
+            .iter()
+            .map(|value| match value {
+                Snafu::Two => '2',
+                Snafu::One => '1',
+                Snafu::Zero => '0',
+                Snafu::Minus => '-',
+                Snafu::DoubleMinus => '=',
+            })
+            .join("");
+
+        write!(f, "{}", result)
+    }
+}
+
+impl Into<isize> for SnafuNumber {
+    fn into(self) -> isize {
+        let length = self.0.len();
+        self.0
+            .iter()
+            .enumerate()
+            .map(|(index, value)| {
+                5_isize.pow((length - index - 1) as u32)
+                    * match value {
+                        Snafu::Two => 2,
+                        Snafu::One => 1,
+                        Snafu::Zero => 0,
+                        Snafu::Minus => -1,
+                        Snafu::DoubleMinus => -2,
+                    }
+            })
+            .sum::<isize>()
+    }
+}
+
+impl From<isize> for SnafuNumber {
+    fn from(value: isize) -> Self {
+        let mut number = value;
+        let mut snafu = Vec::new();
+        let mut carry = 0;
+        while number > 0 {
+            let remainder = number % 5 + carry;
+            number = number / 5;
+
+            if number == 0 && remainder == 0 {
+                break;
+            }
+
+            match remainder {
+                1 => {
+                    snafu.push(Snafu::One);
+                    carry = 0;
+                }
+                2 => {
+                    snafu.push(Snafu::Two);
+                    carry = 0;
+                }
+                3 => {
+                    snafu.push(Snafu::DoubleMinus);
+                    carry = 1;
+                }
+                4 => {
+                    snafu.push(Snafu::Minus);
+                    carry = 1;
+                }
+                5 => {
+                    snafu.push(Snafu::Zero);
+                    carry = 1;
+                }
+                _ => {
+                    snafu.push(Snafu::Zero);
+                    carry = 0;
+                }
+            }
+        }
+
+        snafu.reverse();
+
+        SnafuNumber(snafu)
+    }
 }
